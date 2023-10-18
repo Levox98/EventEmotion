86be5de5657578999332219ca5aad82b0be3d212
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-10-18 15:50:14 +0500

    Added edge-to-edge

--- app/src/main/java/com/eventemotion/MainActivity.kt
+++ app/src/main/java/com/eventemotion/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.eventemotion.ui.theme.EventEmotionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             EventEmotionTheme {
                 // A surface container using the 'background' color from the theme
